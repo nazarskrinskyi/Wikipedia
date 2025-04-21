c4c0db0ca7f94b2b28b1d50c7da3bd7eeab9b647
vladmelnykk <vlad.melnyk28@gmail.com>
2025-04-21 19:39:54 +0300

    add: favicon.ico; fix: logo color

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -7,7 +7,7 @@
         <div class='flex items-center'>
             <div class="shrink-0 flex items-center">
                 <a href="{{ route('home.index') }}">
-                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <x-application-logo class="block h-9 w-auto fill-current" />
                 </a>
             </div>
 
